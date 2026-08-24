1d7bfdaaa780b2792789fe91a19272965958dff6
yash-kumar-18 <yashkumartatiri@gmail.com>
2026-08-24 13:12:35 +0530

    Bill_Splitter.cpp

--- Bill_Splitter.cpp
+++ Bill_Splitter.cpp
@@ -4,24 +4,26 @@ int main () {
 
 // making a variable to store value of total bill
 double totalbill;
-
+// making a variable to store value of total persons
+double totalpersons;
+double billperperson;
 // using to tell user to enter Total  Bill Amount
 cout << "Total  Bill Amount = ";
 
 // using to get input from user to know total amount of bill
 cin >> totalbill;
 
-// making a variable to store value of total persons
-double totalpersons;
-
 // using to tell user to enter Total  Persons
 cout << "Total Persons = " ;
 
 // using to get input from user to know total persons
 cin >> totalpersons ;
-
+if (totalpersons==0){
+cout << "Error: Divison With Zero Is Not Allowed";
+return 0;}
 // making a variable to store value of bill per person
-double billperperson = totalbill/totalpersons ;
+else {
+    billperperson = totalbill/totalpersons;}
 
 // using to give output about bill per person
 
