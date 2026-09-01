72428cf8126a007c78311d8fc566fee09111b59e
yash-kumar-18 <yashkumartatiri@gmail.com>
2026-09-01 21:45:46 +0530

    Smart_Card_Fare_System.cpp

--- Smart_Card_Fare_System.cpp
+++ Smart_Card_Fare_System.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
- double cardbalance=200.00;
+    // using variables to save cardbalance,entry,exit points and fare
+int cardbalance=200;
 int entry;
 int exit;
 int fare;
+// Asking user his entry point
 cout << "Choose Your Departure Point Below"<< endl;
 cout << "1. Sector-1" << endl;
  cout << "2. Sector-2" << endl;
@@ -13,10 +15,11 @@ cout << "1. Sector-1" << endl;
  cout << "5. Sector-5" << endl;
  cout << "Enter Departure Point Option No.: ";
  cin >> entry;
+ // stopping user from entering wrong Departure point
  if (!(entry==1||entry==2||entry==3||entry==4||entry==5))
  {cout << "Error: Enter A Valid Input"<< endl;
 return 0;}
- 
+  // Asking user his exit point
 cout << "Choose Your Destination Point Below"<< endl;
 cout << "1. Sector-1" << endl;
  cout << "2. Sector-2" << endl;
@@ -25,27 +28,22 @@ cout << "1. Sector-1" << endl;
  cout << "5. Sector-5" << endl;
  cout << "Enter Destination Point Option No.: ";
  cin >> exit;
+ // stopping user from entering wrong Destination point
 if (!(exit==1||exit==2||exit==3||exit==4||exit==5))
  {cout << "Error: Enter A Valid Input"<< endl;
 return 0;}
+// stopping user from entering same Departure and Destination points
 else if (entry==exit)
 {cout << "Error: Departure And Destination Points Can't Be Same"<<endl;
     return 0;
 }
-if (entry>exit)
-{(fare=(entry-exit)*10);
+// fare calculation
+fare= abs(entry-exit)*10;
     cout <<"Your Total Fare:" << fare<<endl;
+    // stopping user from using more money than cardbalance
     if (cardbalance<fare)
     {cout <<"Error: Insufficient Card Balance" << endl;
     return 0;}
-else
-cout << "Total Card Balance After Fare Deduction: "<< cardbalance-fare;}
-{(fare=(exit-entry)*10);
-    cout <<"Your Total Fare:" << fare<<endl;
-    if (cardbalance<fare)
-    {cout <<"Error: Insufficient Card Balance" << endl;
-    return 0;}
-cout << "Total Card Balance After Fare Deduction: "<< cardbalance-fare;}
-
+cout << "Total Card Balance After Fare Deduction: "<< cardbalance-fare;
     return 0;
 }
