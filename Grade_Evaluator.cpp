642f7184a47764cb7a9b95e4acd81ad9e1e69619
Yash Kumar <yashkumartatiri@gmail.com>
2026-09-16 22:23:55 +0530

    Update Grade_Evaluator.cpp

--- Grade_Evaluator.cpp
+++ Grade_Evaluator.cpp
@@ -9,22 +9,25 @@ int main () {
     
   // using to get input about marks
   cin >> marks;
-
+    // using to stop user to enter wrong input
+    if (marks>100)
+        cout << "Error: Marks Cannot Be Greater Than 100." << endl ;
+else
   // condition
     if (marks>=90)
-    { cout << "Grade A" << endl ;
+    { cout << "Excellent work! You achieved Grade A." << endl ;
     }
     else if (marks>=75)
-    {cout << "Grade B" << endl ;
+    {cout << "Good job! Keep it up — you earned Grade B" << endl ;
     }
     else if (marks>=50)
-    {cout << "Grade C" << endl ;
+    {cout << "You passed with Grade C. With more effort, you can do even better." << endl ;
     }
     else if (marks>=33)
-    {cout << "Grade D (Pass)" << endl ;
+    {cout << "You passed with Grade D. Keep working harder to improve further." << endl ;
     }
     else 
-    {cout << "Grade F (Fail)" << endl ;
+    {cout << "Don't lose hope - you'll have many chances ahead. This time you got Grade F (Fail)." << endl ;
     }
     return 0;
 }
